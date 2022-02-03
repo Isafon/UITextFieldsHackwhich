331b032c09228ab57471d14aa2a6dbcf6b90ec31
Isa Fontana <isfon8@gmail.com>
2022-02-03 14:13:21 -0700

    Final Product

--- UITextFieldsHackwhich/ViewController.swift
+++ UITextFieldsHackwhich/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        nameLabel.text = nameTextField.text
+        nameTextField.resignFirstResponder()
+    }
+    
+    @IBAction func primaryActionTriggered(_ sender: UITextField) {
+        nameLabel.text = nameTextField.text
+        nameTextField.resignFirstResponder()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
         // Do any additional setup after loading the view.
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameLabel.text = nameTextField.text
+        nameTextField.resignFirstResponder()
+        return true
+    }
 
 }
 
